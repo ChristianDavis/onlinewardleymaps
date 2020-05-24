475acb8612fadab72288999bcd5e7b2bdec66011
ChristianDavis <christian@csgnetwork.com>
2020-05-24 14:48:18 -0500

    polyfill array.flat

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { render } from 'react-dom';
 import 'bootstrap/scss/bootstrap.scss';
 import './styles.scss';
 import App from './components/App';
-
+// polyfill for flat < node 11
+import 'core-js/features/array/flat';
 render(<App />, document.getElementById('root'));
